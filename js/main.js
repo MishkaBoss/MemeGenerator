5034d63486f659dbc05cfa62878b3e4598974ee0
Michael Mozaikov <45514035+MishkaBoss@users.noreply.github.com>
2022-06-18 22:18:07 +0300

    last-upload

--- js/main.js
+++ js/main.js
@@ -57,6 +57,20 @@ var gCtx
 function init() {
     gCanvas = document.getElementById('my-canvas')
     gCtx = gCanvas.getContext('2d')
+    // window.addEventListener('resize', resizeCanvas)
+
+}
+function renderMeme(imgId) {
+    var elGallery = document.querySelector('.canvas-editor')
+    if (elGallery.style.display = 'block') {
+        switchToEditor()
+    }
+    var base_image = new Image();
+    base_image.src = `/images/memes/${imgId}.jpg`
+
+    gCtx.drawImage(base_image, 0, 0);
+    drawText(getText(), 50, 60)
+
 }
 function getMeme(url) {
     url.toString()
@@ -70,3 +84,47 @@ function setColor(value) {
     gCurrColor = value
     console.log(value);
 }
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-editor');
+    gCanvas.width = elContainer.offsetWidth - 700;
+
+}
+function switchToEditor() {
+    var elEditor = document.querySelector('.canvas-editor')
+    var elGallery = document.querySelector('.main-content')
+    elEditor.style.display = 'block'
+    elGallery.style.display = 'none'
+}
+function switchToGallery() {
+    var elEditor = document.querySelector('.canvas-editor')
+    var elGallery = document.querySelector('.main-content')
+    elEditor.style.display = 'none'
+    elGallery.style.display = 'block'
+}
+
+function drawText(text, x = 50, y = 60) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '40px impact';
+    gCtx.fillText(text, x, y);//Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(text, x, y);//Draws (strokes) a given text at the given (x, y) position.
+
+    // renderMeme()
+}
+
+function getText(txt) {
+    var txt = document.querySelector('.text-input').value
+    return txt
+
+}
+
+var inputBox = document.getElementById('text-input')
+var currText = document.getElementById('text-input').value
+inputBox.onkeyup = function () {
+    console.log('hi');
+
+    drawText(inputBox.value)
+    document.getElementById('my-canvas').innerHTML = inputBox.value;
+}
+
